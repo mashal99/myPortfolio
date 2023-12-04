a5c450dcca68bd8fe5b28ef76f37688564462809
Ahmed Mashaal <46572957+mashal99@users.noreply.github.com>
2023-12-03 23:42:41 -0500

    Name Corrected

--- my-portfolio/app/components/Navbar.jsx
+++ my-portfolio/app/components/Navbar.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import React, { useState } from "react";
 import NavLink from "./NavLink";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
-import MenuOverlay from "./ManuOverlay";
+import MenuOverlay from "./MenuOverlay";
 
 const navLinks = [
   {
