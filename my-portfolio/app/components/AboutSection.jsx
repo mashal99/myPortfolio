3244de99c3cf5db903d22c4301ec5f8f66db6daa
Ahmed Mashaal <46572957+mashal99@users.noreply.github.com>
2025-11-30 19:45:27 -0500

    Fix image issue

--- my-portfolio/app/components/AboutSection.jsx
+++ my-portfolio/app/components/AboutSection.jsx
@@ -144,8 +144,7 @@ const AboutSection = () => {
           src={AboutImage}
           width={500}
           height={500}
-          alt="Ahmed Mashaal portrait"
-          className="rounded-2xl object-cover"
+          alt="Portrait of Ahmed Mashaal"
         />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
